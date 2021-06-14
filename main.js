4d3a47f5e9aaf338c60199cb0d548ca24e8d571b
CODERBLOXx <78531418+CODERBLOXx@users.noreply.github.com>
2021-06-14 13:28:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,25 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9e8HOSk5X/model.json',modelLoaded);
 }
 
 function draw(){
     image(video,0,00,300,300);
+    classifier.classify(video,gotResult);
+}
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(2);
+    }
 }
